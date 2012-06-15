0197e7d787e8b09760c6d78de436d89d941aea6b
Mathias Bynens <mathias@qiwi.be>
2012-06-15 11:48:41 +0200

    Make generator scripts output data in the new format
    Ref. #29.

---
+++ tools/generate.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Based on http://git.io/unicode by @mathias
+
+# Allow running this script from another directory
+cd "$(dirname "$0")"
+
+# Unicode version
+version="6.1.0"
+
+function download() {
+	url="$1"
+	filename="$2"
+	if [ -s "$filename" ]; then
+		echo "Skipping ${filename}, as it has already been downloaded."
+	else
+		echo "Downloading ${url}..."
+		curl -# "$url" > "$filename"
+	fi
+}
+
+# Download Unicode database for each version
+mkdir -p "data"
+echo "Fetching Unicode data..."
+download "http://unicode.org/Public/${version}/ucd/UnicodeData.txt" "data/${version}-database.txt"
+download "http://unicode.org/Public/${version}/ucd/Scripts.txt" "data/${version}-scripts.txt"
+download "http://unicode.org/Public/${version}/ucd/Blocks.txt" "data/${version}-blocks.txt"
+download "http://unicode.org/Public/${version}/ucd/PropList.txt" "data/${version}-proplist.txt"
+
+# Generate the category output data
+echo "Parsing Unicode v${version} categories..."
+python scripts/category-regex.py "data/${version}-database.txt" > "output/categories.js"
+
+# Generate the scripts output data
+echo "Parsing Unicode v${version} scripts..."
+python scripts/script-regex.py "data/${version}-scripts.txt" > "output/scripts.js"
+
+# Generate the blocks output data
+echo "Parsing Unicode v${version} blocks..."
+python scripts/block-regex.py "data/${version}-blocks.txt" > "output/blocks.js"
+
+echo "Done."
---
+++ tools/scripts/script-regex.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# Based on http://git.io/unicode by @mathias
+
+from utils import *
+import sys
+
+def format(scriptName, bmpRange, astralRange, hasIndependentSets):
+	buf = []
+	if hasIndependentSets:
+		buf.append('        hasIndependentSets: true')
+	if bmpRange != '':
+		buf.append('        bmp: \'' + bmpRange + '\'')
+	if astralRange != '':
+		buf.append('        astral: \'' + astralRange + '\'')
+	return '    {\n        name: \'' + scriptName + '\',\n' + ',\n'.join(buf) + '\n    }'
+
+def main(sourceFile):
+	dictionary = parseScriptsOrProps(sourceFile)
+	buf = []
+	for item in sorted(dictionary.items()):
+		script = item[0]
+		ranges = createRange(item[1])
+		buf.append(format(script, ranges[0], ranges[1], ranges[2]))
+	print '[\n' + ',\n'.join(buf) + '\n]'
+
+if __name__ == '__main__':
+	main(sys.argv[1])
