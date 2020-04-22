95eeebeb8fac8754d54eafe2b4743661ac1cf028
Anthony Rainer <pristine.source@gmail.com>
2020-04-21 21:26:59 -0500

    Updated types def and tests (#288)
    Created here from https://github.com/DefinitelyTyped/DefinitelyTyped/pull/43115 as per request from @jfahrenkrug

--- types/test.ts
+++ types/test.ts
@@ -1,173 +1,283 @@
-/**
- * Ported from https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/xregexp/xregexp-tests.ts
- */
-
 import XRegExp = require('xregexp');
-import TokenOpts = XRegExp.TokenOpts;
 
-// --  --  --  --  --  --  --  --  --  --  --  --  --
+// ======================================================
+// constructor
+// ======================================================
+let regex1: RegExp = /a/gi;
+regex1 = XRegExp('/a/');
+regex1 = XRegExp('/a/', 'gi');
+regex1 = XRegExp(/a/gi);
+regex1 = XRegExp(regex1, undefined);
 
-let exp: RegExp = /test/;
-const expArr: RegExp[] = [];
-const expArrArr: RegExp[][] = [];
-const strOrExpArr: Array<string | RegExp> = [];
-const chain: RegExp[] = [];
-const groupChain: Array<{ regex: RegExp; backref: string }> = [];
-const groupChain1: Array<{ regex: RegExp; backref: number }> = [];
-let regex: RegExp;
-const value: any = "test";
-let str: string = "";
-const scope: string = "";
-const search: string = "";
-const searchEx: RegExp = /test/;
-let bool: boolean;
-let strArr: string[] = [];
-let strOrStrArr: string | string[];
-const strArrArr: string[][] = [];
-const pattern: string = "";
-const subs: { [name: string]: RegExp } = {};
-const flags: string = "";
-const right: string = "";
-const left: string = "";
-const sticky: boolean = false;
-const pos: number = 0;
-let num: number;
-const limit: number = 1;
-const obj: object = {};
-let matchArr: RegExpExecArray;
-const options: TokenOpts = {};
-const replacer: (...args: any[]) => string = () => "";
+// ======================================================
+// XRegExp namespace
+// ======================================================
 
-// --  --  --  --  --  --  --  --  --  --  --  --  --
+//#region types
 
-regex = XRegExp(str);
-regex = XRegExp(str, flags);
-regex = XRegExp(regex);
+// TokenScope
+let ts1: XRegExp.TokenScopeOption = 'default';
+ts1 = 'class';
+ts1 = 'all';
 
-str =  XRegExp.version;
+// MatchScope
+let ms: XRegExp.MatchScope = 'one';
+ms = 'all';
 
-// --  --  --  --  --  --  --  --  --  --  --  --  --
+// TokenFlag
+let tf: XRegExp.TokenFlag = 'A';
+tf = 'a';
+tf = '0';
+tf = '_';
+tf = '$';
 
-regex = XRegExp(str);
-regex = XRegExp(str, flags);
-regex = XRegExp(regex);
+// Feature
+let fo: XRegExp.FeatureOptions = 'astral';
+fo = 'namespacing';
+fo = 'astral namespacing';
+fo = 'namespacing astral';
+fo = {};
+fo = { astral: true };
+fo = { namespacing: true };
+fo = { astral: true, namespacing: true };
 
-str =  XRegExp.version;
+// Pattern
+let pat: XRegExp.Pattern = '/a/';
+pat = /a/gi;
 
-// --  --  --  --  --  --  --  --  --  --  --  --  --
+// NamedGroups
+let ng: XRegExp.NamedGroupsArray = {};
 
-XRegExp.addToken(regex, (arr, scope) => {
-	matchArr = arr;
-	str = scope;
-	return str;
-});
+// MatchChainArray
+const mca: XRegExp.MatchChainArray = [];
+mca[0] = /a/gi;
 
-XRegExp.addToken(regex, (arr, scope) => {
-	matchArr = arr;
-	str = scope;
-	return str;
-}, options);
+// ReplacementValue
+let rv: XRegExp.ReplacementValue = (s, args) => 'a';
+rv = 'a';
 
-// --  --  --  --  --  --  --  --  --  --  --  --  --
+// UnicodeCharacterRange
+let ucr: XRegExp.UnicodeCharacterRange = { name: 'a', astral: 'a-z' };
+ucr = { name: 'b', bmp: 'a-z' };
+ucr = { name: 'b', inverseOf: 'a-z' };
 
-XRegExp.addUnicodeData([
-    { name: "Test" },
-    { name: "test2", inverseOf: "Test" },
-]);
+//#endregion
 
-// --  --  --  --  --  --  --  --  --  --  --  --  --
+//#region interfaces
 
-regex = XRegExp.build(pattern, strArr, flags);
-regex = XRegExp.build(pattern, strArr);
-regex = XRegExp.build(pattern, subs, flags);
-regex = XRegExp.build(pattern, subs);
-regex = XRegExp.cache(pattern);
-regex = XRegExp.cache(pattern, flags);
+// TokenOptions
+const to: XRegExp.TokenOptions = {};
+to.scope = ts1;
+to.flag = tf;
+to.optionalFlags = 'gi';
+to.reparse = false;
+to.leadChar = '_';
 
-str = XRegExp.escape(str);
+// NamedGroupsArray
+ng = { name: 'string1', val: 'string2' };
+const ng_str1: string = ng['name'] + ng['val'];
 
-matchArr = XRegExp.exec(str, regex, pos, sticky);
-matchArr = XRegExp.exec(str, regex, pos);
-matchArr = XRegExp.exec(str, regex);
+// MatchArray
+class XRegExpMatchArrayImpl extends Array<string> implements XRegExp.MatchArray {
+    constructor(...items: string[]) {
+        super(...items);
+        Object.setPrototypeOf(this, Object.create(XRegExpMatchArrayImpl.prototype));
+    }
+    groups?: XRegExp.NamedGroupsArray;
+    input?: string;
+    index?: number;
+}
+let ma: XRegExp.MatchArray = new XRegExpMatchArrayImpl('asdf', 'qwerty');
+ma.index = 0;
+ma.input = 'a';
+ma.groups = ng;
+ma['namedMatch'] = 'b';
+const ma_str1: string | undefined = ma['namedMatch'] as string;
 
-// --  --  --  --  --  --  --  --  --  --  --  --  --
+// ExecArray
+class XRegExpExecArrayImpl extends Array<string> implements XRegExp.ExecArray {
+    constructor(...items: string[]) {
+        super(...items);
+        Object.setPrototypeOf(this, Object.create(XRegExpExecArrayImpl.prototype));
+    }
+    groups?: XRegExp.NamedGroupsArray;
+    input = '';
+    index = 0;
+}
+const ea: XRegExp.ExecArray = new XRegExpExecArrayImpl('asdf', 'qwerty');
+ea.groups = ng;
+ma.index = 0;
+ma.input = 'a';
+ea['namedMatch'] = 'b';
+const ea_str1: string | undefined = ea['namedMatch'] as string;
 
-XRegExp.forEach(str, regex, (match, index, input, regexp) => {
-	exp = regexp;
-	str = input;
-	num = index;
-	matchArr = match;
-});
+// ChainArrayElement
+mca[1] = { regexp: /a/gi, backref: 1 };
+mca[2] = { regexp: /a/gi, backref: 'asdf' };
 
-// --  --  --  --  --  --  --  --  --  --  --  --  --
+// MatchSubString
+class XRegExpMatchSubstringImpl extends String implements XRegExp.MatchSubString {
+    constructor(value?: any) {
+        super(value);
+        Object.setPrototypeOf(this, Object.create(XRegExpMatchSubstringImpl.prototype));
+    }
+    groups?: XRegExp.NamedGroupsArray;
+}
+const mss: XRegExp.MatchSubString = new XRegExpMatchSubstringImpl('asdf');
+mss.groups = ng;
+mss['namedMatch'] = 'b';
+const mss_str1: string | undefined = mss['namedMatch'] as string;
 
-regex = XRegExp.globalize(regex);
+// ReplacementDetail
+let rd: XRegExp.ReplacementDetail = [/a/gi, rv];
+rd = [/a/gi, rv, null];
+rd = [/a/gi, rv, ms];
+rd = [/a/gi, rv, ms, 'undefined indexes will be ignored'];
 
-XRegExp.install(str);
-XRegExp.install(obj);
+// UnionOptions
+const uo: XRegExp.UnionOptions = {};
+uo.conjunction = null;
+uo.conjunction = 'or';
+uo.conjunction = 'none';
 
-bool = XRegExp.isInstalled(str);
-bool = XRegExp.isRegExp(value);
+// MatchRecursiveOptions
+const mro: XRegExp.MatchRecursiveOptions = {};
+mro.escapeChar = null;
+mro.escapeChar = '\\';
+mro.valueNames = null;
 
-strOrStrArr = XRegExp.match(str, regex);
-strOrStrArr = XRegExp.match(str, regex, scope);
-str = XRegExp.match(str, regex, "one");
+// MatchRecursiveValueNames
+const mrvn: XRegExp.MatchRecursiveValueNames = [null, null, null, null, 'undefined indexes will be ignored'];
+mrvn[0] = 'pre';
+mrvn[1] = 'left';
+mrvn[2] = 'inside';
+mrvn[3] = 'right';
+mro.valueNames = mrvn;
 
-strArr = XRegExp.matchChain(str, chain);
-strArr = XRegExp.matchChain(str, groupChain);
-strArr = XRegExp.matchChain(str, groupChain1);
+// MatchRecursiveValueNameMatch
+const mrvnm: XRegExp.MatchRecursiveValueNameMatch = { name: 'a', value: 'a', start: 0, end: 1 };
 
-// --  --  --  --  --  --  --  --  --  --  --  --  --
+// UnicodeCharacterRangeBase
+ucr.alias = 'asdf';
+ucr.isBmpLast = true;
 
-strArr = XRegExp.matchRecursive(str, left, right, flags, options);
-strArr = XRegExp.matchRecursive(str, left, right, flags);
-strArr = XRegExp.matchRecursive(str, left, right);
+//#endregion
 
-// --  --  --  --  --  --  --  --  --  --  --  --  --
-str = XRegExp.replace(str, search, str, scope);
-str = XRegExp.replace(str, search, str);
-str = XRegExp.replace(str, search, replacer, scope);
-str = XRegExp.replace(str, search, replacer);
+//#region constants
 
-str = XRegExp.replace(str, searchEx, str, scope);
-str = XRegExp.replace(str, searchEx, str);
-str = XRegExp.replace(str, searchEx, replacer, scope);
-str = XRegExp.replace(str, searchEx, replacer);
+const version: string =  XRegExp.version;
 
-// --  --  --  --  --  --  --  --  --  --  --  --  --
-str = XRegExp.replaceEach(str, expArrArr);
-str = XRegExp.replaceEach(str, strArrArr);
-str = XRegExp.replaceEach(str, [[str, exp], [str, exp]]);
+//#endregion
 
-// --  --  --  --  --  --  --  --  --  --  --  --  --
+//#region methods
 
-strArr = XRegExp.split(str, search, limit);
-strArr = XRegExp.split(str, search);
-strArr = XRegExp.split(str, searchEx, limit);
-strArr = XRegExp.split(str, searchEx);
+// addToken
+XRegExp.addToken(/a/gi, (m, s, f) => 'a');
+XRegExp.addToken(/b/gi, (m, s, f) => 'b', to);
 
-// --  --  --  --  --  --  --  --  --  --  --  --  --
+// addUnicodeData
+XRegExp.addUnicodeData([ ucr ]);
 
-const h12 = /1[0-2]|0?[1-9]/;
-const h24 = /2[0-3]|[01][0-9]/;
-regex = XRegExp.tag('x')`${h12} : | ${h24}`;
+// build
+regex1 = XRegExp.build('(?x)^ {{v1}}:{{v2}} $', { v1: /a/gi, v2: regex1 });
+regex1 = XRegExp.build('(?x)^ {{v1}}:{{v2}} $', { v1: /a/gi, v2: '/a/' }, 'gi');
 
-// --  --  --  --  --  --  --  --  --  --  --  --  --
+// cache
+regex1 = XRegExp.cache('/a/', 'gi');
 
-bool = XRegExp.test(str, regex, pos, bool);
-bool = XRegExp.test(str, regex, pos);
-bool = XRegExp.test(str, regex);
+// escape
+const escape_str: string = XRegExp.escape('?<.abcde> asdf');
 
-// --  --  --  --  --  --  --  --  --  --  --  --  --
+// exec
+let ean: XRegExp.ExecArray | null = XRegExp.exec('abcdefghijklm', /a/gi);
+ean = XRegExp.exec('abcdefghijklm', /a/gi, 0);
+ean = XRegExp.exec('abcdefghijklm', /a/gi, 0, true);
+ean = XRegExp.exec('abcdefghijklm', /a/gi, 0, 'sticky');
 
-XRegExp.uninstall(obj);
-XRegExp.uninstall(str);
+// forEach
+XRegExp.forEach('ab_ab_ab', /ab/gi, (m, i, s, r) => { /* do action */ });
 
-regex = XRegExp.union(strArr, flags);
-regex = XRegExp.union(strArr);
-regex = XRegExp.union(expArr, flags);
-regex = XRegExp.union(expArr);
-regex = XRegExp.union(strOrExpArr, flags);
-regex = XRegExp.union(strOrExpArr);
+// globalize
+regex1 = XRegExp.globalize(/a/gi);
 
-// --  --  --  --  --  --  --  --  --  --  --  --  --
+// install
+XRegExp.install('astral');
+XRegExp.install('astral namespacing');
+XRegExp.install('namespacing');
+XRegExp.install('namespacing astral');
+XRegExp.install({});
+XRegExp.install({ astral: true });
+XRegExp.install({ namespacing: true });
+XRegExp.install({ astral: true, namespacing: true });
+
+// isInstalled
+let ii_bool = XRegExp.isInstalled('astral');
+ii_bool = XRegExp.isInstalled('namespacing');
+
+// isRegExp
+let ire_bool: boolean = XRegExp.isRegExp(/a/gi);
+ire_bool = XRegExp.isRegExp(null);
+ire_bool = XRegExp.isRegExp(undefined);
+ire_bool = XRegExp.isRegExp('a');
+ire_bool = XRegExp.isRegExp(0);
+ire_bool = XRegExp.isRegExp([]);
+ire_bool = XRegExp.isRegExp({});
+
+// match
+const m_str: string|null = XRegExp.match('asdf', /a/gi, 'one');
+const m_strarr: string[] = XRegExp.match('asdf', /a/gi, 'all');
+const m_any: string|null|string[] = XRegExp.match('asdf', /a/gi);
+
+// matchChain
+ma = XRegExp.matchChain('asdf', mca);
+
+// matchRecursive
+let mr1: string[]
+    = XRegExp.matchRecursive('asdf', 'a', 'f');
+mr1 = XRegExp.matchRecursive('asdf', 'a', 'f', 'gi');
+let mr2: XRegExp.MatchRecursiveValueNameMatch[]
+    = XRegExp.matchRecursive('asdf', 'a', 'f', null, { valueNames: [ 'a', 'b', 'c', 'd' ] });
+mr2 = XRegExp.matchRecursive('asdf', 'a', 'f', 'gi', { valueNames: [ 'a', 'b', 'c', 'd' ] });
+
+// replace
+let r_str: string = XRegExp.replace('asdf', '/a/', 'b');
+r_str = XRegExp.replace('asdf', /a/gi, (s, args) => 'a', 'all');
+r_str = XRegExp.replace('asdf', /a/gi, (s, args) => 'a', 'one');
+
+// replaceEach
+const re_str: string = XRegExp.replaceEach('asdf', [ rd ]);
+
+// split
+let s_strarr: string[] = XRegExp.split('asdf', '/a/');
+s_strarr = XRegExp.split('asdf', /a/gi, 2);
+
+// tag
+let tag_re: RegExp = /a/g;
+tag_re = XRegExp.tag('i')`(asdf|${tag_re}|qwerty)`;
+
+// test
+let t_bool: boolean = XRegExp.test('asdf', '/a/');
+t_bool = XRegExp.test('asdf', /a/gi, 3);
+t_bool = XRegExp.test('asdf', '/a/', undefined, true);
+t_bool = XRegExp.test('asdf', /a/gi, 1, 'sticky');
+
+// uninstall
+XRegExp.uninstall('astral');
+XRegExp.uninstall('astral namespacing');
+XRegExp.uninstall('namespacing');
+XRegExp.uninstall('namespacing astral');
+XRegExp.uninstall({});
+XRegExp.uninstall({ astral: true });
+XRegExp.uninstall({ namespacing: true });
+XRegExp.uninstall({ astral: true, namespacing: true });
+
+// union
+let u_re: RegExp = XRegExp.union([ '/a/', /b/gi, XRegExp(/a/gi) ]);
+u_re = XRegExp.union([ '/a/', /b/gi, XRegExp(/a/gi) ], null);
+u_re = XRegExp.union([ '/a/', /b/gi, XRegExp(/a/gi) ], 'gi');
+u_re = XRegExp.union([ '/a/', /b/gi, XRegExp(/a/gi) ], 'gi', { });
+u_re = XRegExp.union([ '/a/', /b/gi, XRegExp(/a/gi) ], 'gi', { conjunction: null });
+u_re = XRegExp.union([ '/a/', /b/gi, XRegExp(/a/gi) ], 'gi', { conjunction: 'or' });
+u_re = XRegExp.union([ '/a/', /b/gi, XRegExp(/a/gi) ], 'gi', { conjunction: 'none' });
+
+//#endregion
