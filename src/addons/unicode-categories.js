1e8e3fa0deea43176269d9e2a2b4946a187ad467
Joseph Frazier <1212jtraceur@gmail.com>
2020-12-08 00:28:23 -0500

    Bump version to 4.4.1 (#312)
    Changes include:
    
    * Add browser field to package.json to fix webpack: https://github.com/slevithan/xregexp/pull/308
    
    To generate this commit, I adapted the steps at https://github.com/slevithan/xregexp/pull/205#issuecomment-354236652
    
    > Here's a fuller list of changes that can be needed with new releases:
    > 
    > * Version number
    >   * Update version number and year in headers, config files, README.
    >   * Update year in LICENSE.
    >   * Update version number in `XRegExp.version`.
    > * Publish
    >   * Publish new git tag. E.g.:
    >     * `git tag -a v3.1.0 -m "Release 3.1.0"`.
    >     * `git push origin v3.1.0`.
    >   * `npm publish`.

--- src/addons/unicode-categories.js
+++ src/addons/unicode-categories.js
@@ -1,5 +1,5 @@
 /*!
- * XRegExp Unicode Categories 4.4.0
+ * XRegExp Unicode Categories 4.4.1
  * <xregexp.com>
  * Steven Levithan (c) 2010-present MIT License
  * Unicode data by Mathias Bynens <mathiasbynens.be>
