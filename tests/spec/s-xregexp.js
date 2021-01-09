c68536ee575461eb64c743e6fca558f0da37a364
Joseph Frazier <1212jtraceur@gmail.com>
2021-01-09 14:56:40 -0500

    Don't disallow numbered backreferences like \k<1>, see https://github.com/slevithan/xregexp/pull/247/files#r554340317

--- tests/spec/s-xregexp.js
+++ tests/spec/s-xregexp.js
@@ -485,10 +485,6 @@ describe('XRegExp()', function() {
                 expect(function() {XRegExp('\\k<`>');}).toThrowError(SyntaxError);
             });
 
-            it('should not allow leading digits', function() {
-                expect(function() {XRegExp('(.)\\k<01>');}).toThrowError(SyntaxError);
-            });
-
             it('should separate backreferences from following literal digits', function() {
                 expect(XRegExp('(?<$1>A1)(2)(3)(4)(5)(6)(7)(8)(9)(B10)\\k<$1>0').test('A123456789B10A10')).toBe(true);
                 expect(XRegExp('(?<$1>A)\\k<$1>2').test('AA2')).toBe(true);
